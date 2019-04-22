adb85cd8104d91b703f124e1c6b627d83fa87680
NoBeerJustDeer <49005281+NoBeerJustDeer@users.noreply.github.com>
2019-04-22 19:05:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+	    default: "arcade",
+	    arcade: {
+	    	gravity: {y, 300},
+		debug: false
+	    }
+    },	
     scene: {
         preload: preload,
         create: create,
@@ -16,9 +23,16 @@ function preload ()
 	this.load.image("sky", "assets/Background.png");
 }
 
+var platforms;
+
 function create ()
 {
 	this.add.image(400, 300, "sky");
+	
+	platforms = this.physics.add.staticGroup();
+	
+	platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+	platforms.create(600, 400, 'ground');
 }
 
 function update ()
